df7cd73e3874417ee823427d9010c47f1ca9edb1
puc_dong <38744096+1417766861@users.noreply.github.com>
2019-01-13 22:39:40 +0800

    flask-mail发送邮件

---
+++ flask_emal_demo/config.py
@@ -0,0 +1,11 @@
+#encoding:utf-8
+# __author__ = 'donghao'
+# __time__ = 2019/1/13 21:38
+
+# 邮箱
+MAIL_SERVER = "smtp.qq.com"
+MAIL_PORT = "587"
+MAIL_USE_TLS = True
+MAIL_USERNAME = "pepper.hot@qq.com"
+MAIL_PASSWORD = "saermqhvzvavgfda"
+MAIL_DEFAULT_SENDER = "pepper.hot@qq.com"
---
+++ flask_emal_demo/flask_emal_demo.py
@@ -0,0 +1,64 @@
+from flask import Flask,render_template
+from flask_mail import Mail,Message
+import config
+import os
+path = os.path.dirname(__file__)
+filename = os.path.join(path,'test.jpg')
+
+mail = Mail()
+
+app = Flask(__name__)
+app.config.from_object(config)
+
+mail.init_app(app)
+
+#发送文本
+@app.route('/email_send_charactor/')
+def email_send_charactor():
+    message = Message(subject='hello flask-mail',recipients=['1417766861@qq.com'],body='flask-mail测试代码')
+    try:
+        mail.send(message)
+        return '发送成功，请注意查收~'
+    except Exception as e:
+        print(e)
+        return '发送失败'
+
+
+#发送一个html
+@app.route('/email_send_html/')
+def email_send_html():
+    message = Message(subject='hello flask-mail',recipients=['1417766861@qq.com'])
+    try:
+        #发送渲染一个模板
+        message.html = render_template('email_temp.html')
+        mail.send(message)
+        return '发送成功，请注意查收~'
+    except Exception as e:
+        print(e)
+        return '发送失败'
+
+
+#发送附带附件的邮件
+@app.route('/email_send_attach/')
+def email_send_attach():
+    message = Message(subject='hello flask-mail',recipients=['1417766861@qq.com'],body='我是一个附件邮件')
+    try:
+        # message.attach邮件附件添加
+        # 方法attach(self,
+        #        filename=None,
+        #        content_type=None,
+        #        data=None,
+        #        disposition=None,
+        #        headers=None):
+
+        with open(filename,'rb') as fp:
+            message.attach("test.jpg", "image/jpg", fp.read())
+        mail.send(message)
+        return '发送成功，请注意查收~'
+    except Exception as e:
+        print(e)
+        return '发送失败'
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
