fac8beed97ed4834d05e9ddb0a35fdb3fe74558e
puc_dong <38744096+1417766861@users.noreply.github.com>
2019-01-13 22:45:37 +0800

    flask-mail发送邮件

--- flask_emal_demo/config.py
+++ flask_emal_demo/config.py
@@ -7,5 +7,13 @@ MAIL_SERVER = "smtp.qq.com"
 MAIL_PORT = "587"
 MAIL_USE_TLS = True
 MAIL_USERNAME = "pepper.hot@qq.com"
-MAIL_PASSWORD = "saermqhvzvavgfda"
+MAIL_PASSWORD = "xxxxxx" #生成的授权码
 MAIL_DEFAULT_SENDER = "pepper.hot@qq.com"
+
+
+# MAIL_SERVER	localhost	电子邮件服务器的主机名或IP地址
+# MAIL_PORT	587	电子邮件服务器的端口
+# MAIL_USE_TLS	False	启用传输层安全协议
+# MAIL_USE_SSL	False	启用安全套接层协议
+# MAIL_USERNAME	None	邮件账户的用户名
+# MAIL_PASSWORD	None	邮件账户的密码
